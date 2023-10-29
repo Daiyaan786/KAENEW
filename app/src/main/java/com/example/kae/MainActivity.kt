aedf4b51a03ec3cc2109ad7bcb24b2146952eb88
Daiyaan786 <st10082008@vcconnect.edu.za>
2023-10-29 18:56:57 +0200

    update

--- app/src/main/java/com/example/kae/MainActivity.kt
+++ app/src/main/java/com/example/kae/MainActivity.kt
@@ -4,16 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kae.R.id.button3
+import com.example.kae.R.id.buttonRegister
+import com.example.kae.R.layout.mainpage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mainpage)
+        setContentView(mainpage)
 
         // Handle navigation to the login and registration activities here
-        val loginButton = findViewById<View>(R.id.button3)
-        val registerButton = findViewById<View>(R.id.buttonRegister)
+        val loginButton = findViewById<View>(button3)
+        val registerButton = findViewById<View>(buttonRegister)
 
         loginButton.setOnClickListener {
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
