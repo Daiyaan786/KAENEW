aa54d160cb8d9fd207966ab4f357342d392307e6
Daiyaan786 <st10082008@vcconnect.edu.za>
2023-11-01 18:10:04 +0200

    update

---
+++ app/src/main/java/com/example/kae/Page15.kt
@@ -0,0 +1,12 @@
+package com.example.kae
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Page15 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page15)
+    }
+}
---
+++ app/src/main/java/com/example/kae/Page17.kt
@@ -0,0 +1,12 @@
+package com.example.kae
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Page17 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page17)
+    }
+}
